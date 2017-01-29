c99943765e22ab681df90607fbc935bf17168ad3
Josantonius <info@josantonius.com>
2017-01-30 00:59:34 +0100

    Repository version updated and the minimum required PHP version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # CHANGELOG
 
+## 1.1.0 - 2017-01-30
+* Compatible with PHP 5.6 or higher.
+
+## 1.0.0-v - 2017-01-30
+* Compatible only with PHP 7.0 or higher. In the next versions, the library will be modified to make it compatible with PHP 5.6 or higher.
+
 ## 1.0.0 - 2016-12-15
 * Added `Josantonius\Cookie\Cookie` class.
 * Added `Josantonius\Cookie\Cookie::set()` method.
--- tests/CookieTest.php
+++ tests/CookieTest.php
@@ -8,9 +8,9 @@
  * @author     Josantonius - info@josantonius.com
  * @copyright  Copyright (c) 2016 JST PHP Framework
  * @license    https://opensource.org/licenses/MIT - The MIT License (MIT)
- * @version    1.0.0
+ * @version    1.1.0
  * @link       https://github.com/Josantonius/PHP-Cookie
- * @since      File available since 1.0.0 - Update: 2016-12-15
+ * @since      File available since 1.0.0 - Update: 2017-01-30
  */
 
 namespace Josantonius\Cookie\Tests;
